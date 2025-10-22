f5c1a5adc08919fb6e4facc234a5d86f54363832
Chika <akkaihcchi@gmail.com>
2025-10-22 10:19:34 +0900

    fix MemoModel timestamp

--- app/Models/MemoModel.php
+++ app/Models/MemoModel.php
@@ -13,22 +13,24 @@ class MemoModel extends Model
     protected $table = 'memos'; //Model名変更のため記述
 
     /**
-     * 現在時刻を基準にした日時情報を種別によって返す
+     * 現在時刻を基準にした日時情報を日時種別によって返す
      * 
-     * @param string $type 1=今月末の日付, 2=現在時刻の2時間前, 3=来月末の日付
+     * @param string $type 日時種別
+     * @param string $timestamp 呼び元で生成したタイムスタンプ
+     * @return string 日時(YYYY-MM-DD HH:mm:ss形式)
      */
 
-    public static function getDateTimes(string $type){
+    public static function getDateTimes($type, $timestamp = null){
 
-        $now = Carbon::now();
+        $now = $timestamp ? Carbon::parse($timestamp) : Carbon::now(); //呼び元から渡されたTimeStampを使用
         switch($type){
-            case MemoConst::DATE_END_OF_THIS_MONTH: //今月末の日付
-                return $now->copy()->endOfMonth()->format('Y-m-d'); 
+            case MemoConst::DATE_END_OF_THIS_MONTH: //今月末
+                return $now->copy()->endOfMonth()->format('Y-m-d H:i:s'); 
             case MemoConst::DATE_TWO_HOURS_AGO: //現在時刻の2時間前
                 return $now->copy()->subHours(2)->format('Y-m-d H:i:s'); 
-            case MemoConst::DATE_END_OF_NEXT_MONTH: //来月末の日付
-                return $now->copy()->addMonthNoOverflow()->endOfMonth()->format('Y-m-d');
-            default: //1~3以外が来た場合は現在日時
+            case MemoConst::DATE_END_OF_NEXT_MONTH: //来月末
+                return $now->copy()->addMonthNoOverflow()->endOfMonth()->format('Y-m-d H:i:s');
+            default: //上記以外が来た場合は現在日時
                 return $now->format('Y-m-d H:i:s');
         }
     }
